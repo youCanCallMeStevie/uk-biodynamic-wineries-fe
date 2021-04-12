94317bd816b13915dde776d5db1097987e74e972
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-04-12 12:03:19 +0100

    icon banner for mobile

--- src/Components/IconBanner/IconBanner.elements.tsx
+++ src/Components/IconBanner/IconBanner.elements.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const IconSection = styled.div`
   padding: 50px 0;
   background-color: #f3f4ed;
+  width: 100%;
 `;
 export const IconsContainer = styled.div`
   display: flex;
@@ -13,9 +14,16 @@ export const IconsContainer = styled.div`
 export const IconWrap = styled.div`
   color: #101522;
   font-size: 50px;
+  margin-left: 10px;
+  @media screen and (max-width: 775px) {
+    font-size: 30px;
+  } ;
 `;
 export const IconTitle = styled.div`
   font-size: 18px;
   line-height: 16px;
   letter-spacing: 1.4px;
+  @media screen and (max-width: 775px) {
+    font-size: 16px;
+  } ;
 `;
